13352d5bb2f2751db6352bc959729b1177ca21ba
justinthareja <justin.thareja@gmail.com>
2015-05-25 16:11:38 -0700

    GET messages from Parse and styling to display messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,28 @@
 // YOUR CODE HERE:
+$.ajax({
+  url: "https://api.parse.com/1/classes/chatterbox",
+  type: 'GET',
+  dataType:'json',
+  success: function(data){
+  var messages = data.results;
+  // get returns object
+  for (var i = 0; i < messages.length; i++) {
+    var username = messages[i].username;
+    var message = messages[i].text;
+    var $textContainer = $('<li class="list-group-item"></li>')
+
+    $textContainer.text(username +': ' + message);
+    $('.list-group').append($textContainer);
+
+  }
+    // parse object for all messages
+    // iterate over object.results
+    // within each iteration capture the element.username field and element.text field
+    // concat them together in a string and add it to a list item within .container
+    console.log(message);
+  }
+})
+
+// function
+
 
