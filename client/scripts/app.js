78cf752beb1d3d7b4db9ca330ab1ab706f7101b5
justinthareja <justin.thareja@gmail.com>
2015-05-25 17:06:41 -0700

    allows users to post a comment

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,41 +1,63 @@
-// YOUR CODE HERE:
+$(document).ready(function() {
 
-var getMessages = function(){
-    $.ajax({
-    url: "https://api.parse.com/1/classes/chatterbox",
-    type: 'GET',
-    dataType:'json',
-    success: function(data) {
-      $('li').remove();
-      var messages = data.results;
-      // console.log(data);
 
-      for (var i = 0; i < 15; i++) {
-        var username = messages[i].username;
-        var message = messages[i].text;
-        var $textContainer = $('<li class="list-group-item"></li>');
-        $textContainer.text(username +': ' + message);
-        $('.list-group').append($textContainer);
 
+
+  var getMessages = function(){
+      $.ajax({
+      url: "https://api.parse.com/1/classes/chatterbox",
+      type: 'GET',
+      dataType:'json',
+      success: function(data) {
+        $('li').remove();
+        var messages = data.results;
+        // console.log(data);
+
+        for (var i = 0; i < 15; i++) {
+          var username = messages[i].username;
+          var message = messages[i].text;
+          var $textContainer = $('<li class="list-group-item"></li>');
+          $textContainer.text(username +': ' + message);
+          $('.list-group').append($textContainer);
+
+        }
       }
+    })
+  };
+
+  setInterval(getMessages, 1000);
+
+  var send = function () {
+
+    var userInput = $('#userInput').val();
+    $('#userInput').val('');
+
+    var message = {
+      username: 'something',
+      text: userInput,
+      roomname: 'hrhehe'
     }
-  })
-};
 
-setInterval(getMessages, 1000);
 
-// var parseMessages = function(data) {
-//   var messages = data.results;
-//   console.log(data);
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message');
+      }
+    });
 
-//   for (var i = 0; i < messages.length; i++) {
-//     var username = messages[i].username;
-//     var message = messages[i].text;
-//     var $textContainer = $('<li class="list-group-item"></li>');
-//     $textContainer.text(username +': ' + message);
-//     $('.list-group').append($textContainer);
-//   }
+  }
 
-// };
+  $('#submit').on('click', send)
+
+});
+
+
 
 
